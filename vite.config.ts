7eb2b88d4cdf2012137a70a37b54999f6af59238
Illia Bodnar <illia.v.bodnar@gmail.com>
2025-02-04 21:53:37 +0200

    fix: modify project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     port: 3000,
   },
