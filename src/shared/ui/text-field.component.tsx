860cf771163a85c6985da877b7c0bb89525a1dd5
Illia Bodnar <illia.v.bodnar@gmail.com>
2025-02-05 13:45:26 +0200

    feat: add vite svgr plugin

--- src/shared/ui/text-field.component.tsx
+++ src/shared/ui/text-field.component.tsx
@@ -1,7 +1,7 @@
 import { cn } from "../lib/utils";
 
 const inputDefaultClasses =
-  "ease w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none";
+  "ease w-full flex flex-row rounded-md border border-slate-100 bg-transparent text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none";
 
 type TextFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
@@ -14,11 +14,11 @@ export const TextField: React.FC<TextFieldProps> = ({
   ...props
 }) => {
   return (
-    <div className="flex w-full max-w-sm min-w-[200px] flex-row items-center gap-2">
-      <span className="block text-sm text-slate-600">{label}</span>
+    <div className={cn(inputDefaultClasses, className)}>
+      <div className="rounded-l-md bg-slate-100 px-3 py-2">{label}</div>
       <input
         id={id}
-        className={cn(inputDefaultClasses, className)}
+        className="rounded-r-md px-3 py-2 outline-none"
         {...props}
       />
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), svgr(), tsconfigPaths()],
   server: {
     port: 3000,
   },
